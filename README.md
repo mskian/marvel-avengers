2eed67d81cddcb09f9764815fa82e2d3ff2854e2
mskian <veerfun23@gmail.com>
2018-09-05 12:02:00 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,35 @@
+## Marvel Avengers
+
+<p>Marvel Movies List for Avengers</p>
+
+## Requirements
+
+- Node.JS 6 or 8x
+- GIT to clone this Respo
+
+## Installation
+
+- Install via GIT
+
+```
+git clone https://github.com/mskian/marvel-avengers.git
+cd marvel-avengers
+npm install
+```
+
+- Execute the Script
+
+```
+node app.js
+```
+
+## API
+
+Marvel Avengers Movie List API
+
+```
+https://api.jsonbin.io/b/5b8e93e4db948c68635c80f4/latest
+```
+## License
+
+MIT
---
+++ app.js
@@ -0,0 +1,56 @@
+'use strict';
+
+const axios = require('axios');
+const Table = require('cli-table3');
+const ora = require('ora');
+
+var apiurl = 'https://api.jsonbin.io/b/5b8e93e4db948c68635c80f4/latest';
+
+async function go() {
+
+	const spinner = new ora({
+		text: 'Fetching Marvel Movie List [Avengers]',
+		spinner: 'hamburger'
+	});
+
+	spinner.start();
+
+	setTimeout(() => {
+		spinner.color = 'yellow';
+		spinner.text = 'Successfully Fetched the Movie List';
+	}, 1000);
+
+	try {
+		await new Promise(resolve => setTimeout(resolve, 2000));
+		const wes = await axios(apiurl);
+		var marvel = wes.data;
+		const group = marvel.items.map(g => [
+			g.id,
+			g.title,
+			g.release_date
+		]);
+  
+		const table = new Table({
+			chars: { 'top': '═' , 'top-mid': '╤' , 'top-left': '╔' , 'top-right': '╗'
+				, 'bottom': '═' , 'bottom-mid': '╧' , 'bottom-left': '╚' , 'bottom-right': '╝'
+				, 'left': '║' , 'left-mid': '╟' , 'mid': '─' , 'mid-mid': '┼'
+				, 'right': '║' , 'right-mid': '╢' , 'middle': '│' },
+			style: {head: ['green'],
+				border: ['yellow']},
+			head: ['ID', 'Movie', 'Release Date'],
+			colWidths: [10, 40, 16]
+		});
+
+		table.push(...group);
+		spinner.stop();
+		console.log('\n');
+		console.log(table.toString());
+		console.log('\n');
+
+	} catch (e) {
+		spinner.stop();
+		console.error(e);
+	}
+}
+  
+go();
